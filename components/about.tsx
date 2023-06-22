061acffd525e2aa5a899bb65d24d5eba43a87d52
balits <balits.peter.marton@gmail.com>
2023-06-22 11:59:12 +0200

    max-w to tablet

--- components/about.tsx
+++ components/about.tsx
@@ -1,7 +1,7 @@
 export const About = () => {
   return (
-    <section className="w-full  px-8">
-      <p className="text-2xl my-80">
+    <section className="w-full  px-8 flex items-center justify-center">
+      <p className="text-2xl my-80 max-w-screen-lg md:text-4xl lg:text-5xl">
         <span className="font-bold">People appreciate intuitive design. </span>
         I’m passionate about bringing ideas to life and creating lasting
         impressions.
