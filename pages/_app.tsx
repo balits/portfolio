272c45a939c6ff017315f836e825bc227787267c
Balits Peter <97095806+balits@users.noreply.github.com>
2023-07-08 12:20:56 +0200

    fix hotjar issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,15 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { hotjar } from 'react-hotjar'
-import Script from 'next/script'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    hotjar.initialize(
-      Number(process.env.NEXT_PUBLIC_HJID) || -1,
-      Number(process.env.NEXT_PUBLIC_HJSV) || -2,
-    )
-  }, [])
   return (
     <>
+      <Head>
+        <title>Balits Peter Marton</title>
+      </Head>
       <Component {...pageProps} />
-      <Script
-        id="HOTJAR"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:3565297,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-        }}
-      />
     </>
   )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Balits Peter Marton</title>
         <meta name="language" content="English" />
         <link rel="favicon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon-16x16.png" sizes="16x16" />
@@ -47,6 +47,22 @@ export default function Document() {
         />
         <meta name="twitter:creator" content="@yourtwitterhandle" />
         */}
+        <Script
+          id="HOTJAR"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3565316,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+          }}
+        />
       </Head>
       <body>
         <Main />
