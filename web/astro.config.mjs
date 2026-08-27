30e73e5af06e8a931f87309ed444699b67914540
Bálits Péter <balits.peter@proton.me>
2026-08-27 23:25:22 +0200

    hello, web

---
+++ web/astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig, fontProviders } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  fonts: [{
+    provider: fontProviders.fontsource(),
+    // name: "Source Code Pro",
+    name: "DejaVu Mono",
+    cssVariable: "--monospaced-font",
+  }]
+});
